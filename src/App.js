3d9353d384edf1d84191d500659cbf53e3377ee4
ejioguvictor <ejioguvictor6054@gmail.com>
2022-02-16 02:47:28 +0100

    updated files and cleaned up unused files

--- src/App.js
+++ src/App.js
@@ -21,18 +21,6 @@ function App() {
     }
   }
 
-  // const dateBuilder = (d) => {
-  //   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  //   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-  //   let day = days[d.getDay()];
-  //   let date = d.getDate();
-  //   let month = months[d.getMonth()];
-  //   let year = d.getFullYear();
-
-  //   return `${day} ${date} ${month} ${year}`
-  // }
-
   return (
     <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
       <main>
@@ -48,7 +36,6 @@ function App() {
             <div className="location-box">
               <div className="location">{weather.name},{weather.sys.country}</div>
               <div className="date">{new Date().toDateString()}</div>
-              {/* <div className="date">{dateBuilder(new Date())}</div> */}
             </div>
             <div className="weather-box">
               <div className="temp">{Math.round(weather.main.temp)}°c</div>
